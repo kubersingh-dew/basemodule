c8356ea7b1060b42b29c0dcfc9ebccfab071c528
kuber singh <kubers@dewsolution.in>
2020-04-10 16:12:13 +0530

    minor update

--- src/main/java/com/omni/dew/basemodule/BaseLogin.java
+++ src/main/java/com/omni/dew/basemodule/BaseLogin.java
@@ -1,8 +1,10 @@
 package com.omni.dew.basemodule;
 
+import android.app.Activity;
 import android.content.Intent;
 
 public interface BaseLogin {
+    void init(Activity activity, LoginResponse loginResponse);
     int login();
     void onResponse(Intent data);
     boolean isLogin();
